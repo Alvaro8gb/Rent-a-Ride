43a0135cb85a0ff8941ae9f7c2bc541731e42702
Alvaro <alvaga28@ucm.es>
2023-02-28 16:06:26 +0100

    Add loader to index

--- RentARide/src/main/java/es/ucm/fdi/iw/model/Vehicle.java
+++ RentARide/src/main/java/es/ucm/fdi/iw/model/Vehicle.java
@@ -7,6 +7,7 @@ import java.util.List;
 import lombok.Data;
 
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.GeneratedValue;
