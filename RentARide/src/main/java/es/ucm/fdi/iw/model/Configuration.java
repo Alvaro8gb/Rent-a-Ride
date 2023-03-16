fa9b86a67dc568d03251db84f3fc8fb42071bfca
ggascon <guillermo.gasconsmr@gmail.com>
2023-03-16 16:29:06 +0100

    Renamed configuration class

---
+++ RentARide/src/main/java/es/ucm/fdi/iw/model/Configuration.java
@@ -0,0 +1,29 @@
+package es.ucm.fdi.iw.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.NamedQuery;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NamedQuery(name="Configuration.findAll",
+            query="SELECT c FROM Configuration c")
+
+public class Configuration {
+
+        @Id
+        @Column(nullable = false)
+        private String configKey;
+
+        @Column(nullable = false)
+        private String configValue;
+
+        public Configuration() {
+            
+        }
+}
