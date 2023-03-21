b2d078728bdfcf6f00bac4c0c459cdab82fa3f6b
Álvaro García Barragán <79256834+Alvaro8gb@users.noreply.github.com>
2023-03-21 20:38:33 +0100

    second aproch to show booking

--- RentARide/src/main/java/es/ucm/fdi/iw/controller/BookingController.java
+++ RentARide/src/main/java/es/ucm/fdi/iw/controller/BookingController.java
@@ -16,6 +16,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -75,13 +79,16 @@ public class BookingController {
     }
 
     @GetMapping(path="/{idVehicle}", produces = "application/json")
-    @Transactional
     @ResponseBody
-    public String viewBooking(Model model, @PathVariable long idVehicle){ // Transferable<Booking.Transfer>
-        Booking book = new Booking(null, null, null, null);
+    public String viewBooking(Model model, @PathVariable long idVehicle) throws JsonProcessingException{ // Transferable<Booking.Transfer>
+        Booking book = new Booking(new BookingID(1, 2, null, null), (float)43, null, null);
 
-        log.info("booking {}", book.toString());
-        return book.toString();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(book.toTransfer());
+        
+        log.info("booking {} {}", idVehicle, jsonString);
+
+        return jsonString;
     }
 
 }
--- RentARide/src/main/resources/static/js/booking.js
+++ RentARide/src/main/resources/static/js/booking.js
@@ -1,17 +1,39 @@
 
+function processBooking(response){
+    console.log(response);
+
+    const datos = JSON.stringify(response);
+    const tabla = document.getElementById("content-booking");
+    const encabezado = tabla.createTHead();
+    const filaEncabezado = encabezado.insertRow();
+    for (let dato of datos) {
+    const fila = tabla.insertRow();
+    for (let key in dato) {
+        if (!encabezado.rows[0].cells.namedItem(key)) {
+        const celdaEncabezado = filaEncabezado.insertCell();
+        celdaEncabezado.innerHTML = key;
+        }
+        const celda = fila.insertCell();
+        celda.innerHTML = dato[key];
+  }
+
+  $("#modalAlquiler").modal("show");
+
+  console.log("Fnish");
+
+}
+
+}
+
 function viewBooking(idVehicle) {
     let params = {idVehicle: idVehicle};
     // Spring Security lo añade en formularios html, pero no en Ajax
     params[config.csrf.name] = config.csrf.value;
     // petición en sí
     
-    let info = document.getElementById("modal-content");
-
-    console.log("Ados");
     return go(config.rootUrl + "/booking/"+ idVehicle, 'GET', params)
-        .then(d => info.textContent = "Adios" )
-        .catch(() => "Error showing booking");
-
+        .then(response => processBooking(response) )
+        .catch(e => console.log(e));
     
 }
 
