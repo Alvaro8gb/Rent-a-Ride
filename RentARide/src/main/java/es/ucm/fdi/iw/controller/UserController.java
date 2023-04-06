ba0ccd032831ca162f0324f1d8e363c2a3a4913b
Alvaro <alvaga28@ucm.es>
2023-04-06 14:13:48 +0200

    webs socket 0.1

--- RentARide/src/main/java/es/ucm/fdi/iw/controller/UserController.java
+++ RentARide/src/main/java/es/ucm/fdi/iw/controller/UserController.java
@@ -247,46 +247,18 @@ public class UserController {
 		}
 		return "{\"status\":\"photo uploaded correctly\"}";
     }
-    
-    /**
-     * Returns JSON with all received messages
-     */
-    @GetMapping(path = "received", produces = "application/json")
-	@Transactional // para no recibir resultados inconsistentes
-	@ResponseBody  // para indicar que no devuelve vista, sino un objeto (jsonizado)
-	public List<Message.Transfer> retrieveMessages(HttpSession session) {
-		long userId = ((User)session.getAttribute("u")).getId();		
-		User u = entityManager.find(User.class, userId);
-		log.info("Generating message list for user {} ({} messages)", 
-				u.getUsername(), u.getReceived().size());
-		return  u.getReceived().stream().map(Transferable::toTransfer).collect(Collectors.toList());
-	}	
-    
-    /**
-     * Returns JSON with count of unread messages 
-     */
-	@GetMapping(path = "unread", produces = "application/json")
-	@ResponseBody
-	public String checkUnread(HttpSession session) {
-		long userId = ((User)session.getAttribute("u")).getId();		
-		long unread = entityManager.createNamedQuery("Message.countUnread", Long.class)
-			.setParameter("userId", userId)
-			.getSingleResult();
-		session.setAttribute("unread", unread);
-		return "{\"unread\": " + unread + "}";
-    }
-    
+   
     /**
      * Posts a message to a user.
      * @param id of target user (source user is from ID)
      * @param o JSON-ized message, similar to {"message": "text goes here"}
      * @throws JsonProcessingException
      */
-    @PostMapping("/{id}/msg")
+    @PostMapping(path = "/{id}/msg")
 	@ResponseBody
 	@Transactional
 	public String postMsg(@PathVariable long id, 
-			@RequestBody JsonNode o, Model model, HttpSession session) 
+		@RequestBody JsonNode o, Model model, HttpSession session) 
 		throws JsonProcessingException {
 		
 		String text = o.get("message").asText();
--- RentARide/src/main/resources/static/js/chat.js
+++ RentARide/src/main/resources/static/js/chat.js
@@ -1,8 +1,29 @@
+console.log("Chats");
+ws.receive = (m) => {
+    console.log(m);
+    $(".chatBody").append(
+        '<div class="col-12 chatMessage my-2 py-2 chatReceiver">'+
+        'Mensaje recibido: '+ m["text"] +
+        '</div>');   
+} 
 
-// ws.receive = (m) => {
-//     console.log(m);
-//     $(".chatBody").append(
-//         '<div class="col-12 chatMessage my-2 py-2 chatSender">'+
-//         'Mammahuevo '+ m +
-//         '</div>');   
-// } 
+
+function sendMsg(idReceptor) {
+    let msg = $("#message").val();
+    $("#message").val("");
+    console.log(msg);
+    let params = {message: msg};
+    // Spring Security lo añade en formularios html, pero no en Ajax
+    params[config.csrf.name] = config.csrf.value;
+
+    $(".chatBody").append(
+        '<div class="col-12 chatMessage my-2 py-2 chatSender">'+
+        msg + '</div>');  
+    
+    // petición en sí
+
+    return go(config.rootUrl + "/user/"+ idReceptor + "/msg", 'POST', params)
+        .then(response => console.log(response) )
+        .catch(e => console.log(e));
+    
+}
