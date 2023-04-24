5956fd8dbccfd9b9a964ee3573a414da6de9181f
Morales18 <lpejor8@gmail.com>
2023-04-24 21:01:31 +0200

    tickets
    eliminar ticket, vista con colores en funcion de la gravedad

--- RentARide/src/main/java/es/ucm/fdi/iw/controller/TicketsController.java
+++ RentARide/src/main/java/es/ucm/fdi/iw/controller/TicketsController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.servlet.http.HttpSession;
+import javax.transaction.Transactional;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,7 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import es.ucm.fdi.iw.model.Ticket;
 import es.ucm.fdi.iw.model.User;
@@ -34,5 +38,19 @@ public class TicketsController {
 		model.addAttribute("tickets", pendingMsgs); // Return messages queue
 
 		return "tickets";
+	}
+
+
+	@PostMapping("/{id}/delete")
+	@Transactional
+	public String delete(@PathVariable long id, RedirectAttributes redirAttrs, HttpSession session) {
+		Ticket target = entityManager.find(Ticket.class, id);
+		entityManager.remove(target);
+		entityManager.flush();
+		redirAttrs.addFlashAttribute("successMessage", "El ticket se ha eliminado con éxito");
+
+		// Comprobar si roll admin
+		return "redirect:/incidencias/";
+		
 	}
 }
--- RentARide/src/main/java/es/ucm/fdi/iw/model/Ticket.java
+++ RentARide/src/main/java/es/ucm/fdi/iw/model/Ticket.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.SequenceGenerator;
 
 import lombok.AllArgsConstructor;
@@ -15,6 +17,11 @@ import lombok.Data;
 
 @Data
 @Entity
+@NamedQueries({
+    @NamedQuery(name="Ticket.deleteTicket",
+            query="DELETE FROM Ticket t "
+                    + "WHERE t.id = :id")
+            })
 @AllArgsConstructor
 public class Ticket {
     
